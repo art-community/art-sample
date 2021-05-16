c827a98b600e98483fcec06eebbd7f79a3a8f443
anton.bashirov <anton.sh.local@gmail.com>
2021-05-16 04:09:07 +0300

    ART

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,11 +1,10 @@
 import io.art.logging.module.*;
 import static io.art.launcher.Activator.*;
-import static io.art.logging.module.LoggingActivator.*;
 import static io.art.logging.module.LoggingModule.*;
 
 public class Example {
     public static void main(String[] args) {
-        activator().module(logging(LoggingInitializer::colored)).quiet(true).launch();
+        activator().module(LoggingActivator.logging()).launch();
         logger(Example.class).info("Hello, ART!");
     }
 }
